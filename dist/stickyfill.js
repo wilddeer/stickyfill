34996fbdaf1539ede082645f7e241fd4ea23a863
Oleg Korsunsky <jatruadmin@gmail.com>
2014-06-21 18:39:43 +0400

    tagging 1.0.3

--- dist/stickyfill.js
+++ dist/stickyfill.js
@@ -1,6 +1,6 @@
 /*!
  * Stickyfill -- `position: sticky` polyfill
- * v. 1.0.2 | https://github.com/wilddeer/stickyfill
+ * v. 1.0.3 | https://github.com/wilddeer/stickyfill
  * Copyright Oleg Korsunsky | http://wd.dizaina.net/
  *
  * MIT License
