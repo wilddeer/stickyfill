820c9347017f5a2e0d06c9a7f84b283724a42aaf
Nikita Seleckis <nikita@seleckis.lv>
2017-01-12 11:24:18 +0200

    build

--- dist/stickyfill.js
+++ dist/stickyfill.js
@@ -6,6 +6,9 @@
  * MIT License
  */
 (function(doc, win) {
+	if (typeof doc === 'undefined' || typeof win === 'undefined') {
+		return;
+	}
     var watchArray = [],
         scroll,
         initialized = false,
