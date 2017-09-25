1bebdfbf45b2f1d4ab403da1acef5990658bad86
Oleg Korsunsky <wd@dizaina.net>
2017-09-25 21:50:49 +0300

    API tweaks, code style 🦀

--- src/stickyfill.js
+++ src/stickyfill.js
@@ -43,7 +43,7 @@ const stickies = [];
 /*
  * 3. Utility functions
  */
-function extend(targetObj, sourceObject) {
+function extend (targetObj, sourceObject) {
     for (var key in sourceObject) {
         if (sourceObject.hasOwnProperty(key)) {
             targetObj[key] = sourceObject[key];
@@ -51,11 +51,11 @@ function extend(targetObj, sourceObject) {
     }
 }
 
-function parseNumeric(val) {
+function parseNumeric (val) {
     return parseFloat(val) || 0;
 }
 
-function getDocOffsetTop(node) {
+function getDocOffsetTop (node) {
     let docOffsetTop = 0;
 
     while (node) {
@@ -71,7 +71,7 @@ function getDocOffsetTop(node) {
  * 4. Sticky class
  */
 class Sticky {
-    constructor(node) {
+    constructor (node) {
         if (!(node instanceof HTMLElement))
             throw new Error('First argument must be HTMLElement');
         if (stickies.some(sticky => sticky._node === node))
@@ -86,7 +86,7 @@ class Sticky {
         this.refresh();
     }
 
-    refresh() {
+    refresh () {
         if (seppuku || this._removed) return;
         if (this._active) this._deactivate();
 
@@ -189,7 +189,7 @@ class Sticky {
         this._recalcPosition();
     }
 
-    _recalcPosition() {
+    _recalcPosition () {
         if (!this._active || this._removed) return;
 
         const stickyMode = scroll.top <= this._limits.start? 'start': scroll.top >= this._limits.end? 'end': 'middle';
@@ -242,7 +242,7 @@ class Sticky {
         this._stickyMode = stickyMode;
     }
 
-    _fastCheck() {
+    _fastCheck () {
         if (!this._active || this._removed) return;
 
         if (
@@ -251,7 +251,7 @@ class Sticky {
         ) this.refresh();
     }
 
-    _deactivate() {
+    _deactivate () {
         if (!this._active || this._removed) return;
 
         this._clone.node.parentNode.removeChild(this._clone.node);
@@ -275,7 +275,7 @@ class Sticky {
         delete this._limits;
     }
 
-    remove() {
+    remove () {
         this._deactivate();
 
         stickies.some((sticky, index) => {
@@ -297,8 +297,8 @@ const Stickyfill = {
     stickies,
     Sticky,
 
-    add(node) {
-        // Check whether it’s an node
+    addOne (node) {
+        // Check whether it’s a node
         if (!(node instanceof HTMLElement)) {
             // Maybe it’s a node list of some sort?
             // Take first node from the list then
@@ -316,7 +316,7 @@ const Stickyfill = {
         return new Sticky(node);
     },
 
-    addAll(nodeList) {
+    add (nodeList) {
         // If it’s a node make an array of one node
         if (nodeList instanceof HTMLElement) nodeList = [nodeList];
         // Check if the argument is an iterable of some sort
@@ -326,7 +326,7 @@ const Stickyfill = {
         const addedStickies = [];
 
         for (let i = 0; i < nodeList.length; i++) {
-            let node = nodeList[i];
+            const node = nodeList[i];
 
             // If it’s not an HTMLElement – create an empty element to preserve 1-to-1
             // correlation with input list
@@ -336,7 +336,7 @@ const Stickyfill = {
             }
 
             // If Stickyfill is already applied to the node
-            // and add existing sticky
+            // add existing sticky
             if (stickies.some(sticky => {
                 if (sticky._node === node) {
                     addedStickies.push(sticky);
@@ -351,11 +351,29 @@ const Stickyfill = {
         return addedStickies;
     },
 
-    refreshAll() {
+    refreshAll () {
         stickies.forEach(sticky => sticky.refresh());
     },
 
-    remove(nodeList) {
+    removeOne (node) {
+        // Check whether it’s a node
+        if (!(node instanceof HTMLElement)) {
+            // Maybe it’s a node list of some sort?
+            // Take first node from the list then
+            if (node.length && node[0]) node = node[0];
+            else return;
+        }
+
+        // Remove the stickies bound to the nodes in the list
+        stickies.some(sticky => {
+            if (sticky._node === node) {
+                sticky.remove();
+                return true;
+            }
+        });
+    },
+
+    remove (nodeList) {
         // If it’s a node make an array of one node
         if (nodeList instanceof HTMLElement) nodeList = [nodeList];
         // Check if the argument is an iterable of some sort
@@ -363,7 +381,7 @@ const Stickyfill = {
 
         // Remove the stickies bound to the nodes in the list
         for (let i = 0; i < nodeList.length; i++) {
-            let node = nodeList[i];
+            const node = nodeList[i];
 
             stickies.some(sticky => {
                 if (sticky._node === node) {
@@ -374,7 +392,7 @@ const Stickyfill = {
         }
     },
 
-    removeAll() {
+    removeAll () {
         while (stickies.length) stickies[0].remove();
     }
 };
@@ -383,9 +401,9 @@ const Stickyfill = {
 /*
  * 6. Setup events (unless the polyfill was disabled)
  */
-function init() {
+function init () {
     // Watch for scroll position changes and trigger recalc/refresh if needed
-    function checkScroll() {
+    function checkScroll () {
         if (window.pageXOffset != scroll.left) {
             scroll.top = window.pageYOffset;
             scroll.left = window.pageXOffset;
@@ -411,13 +429,13 @@ function init() {
     //Fast dirty check for layout changes every 500ms
     let fastCheckTimer;
 
-    function startFastCheckTimer() {
-        fastCheckTimer = setInterval(function() {
+    function startFastCheckTimer () {
+        fastCheckTimer = setInterval(function () {
             stickies.forEach(sticky => sticky._fastCheck());
         }, 500);
     }
 
-    function stopFastCheckTimer() {
+    function stopFastCheckTimer () {
         clearInterval(fastCheckTimer);
     }
 
