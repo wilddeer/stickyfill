299036b5c24a410a363dddc86a471af4d28a1dac
Oleg Korsunsky <wd@dizaina.net>
2017-10-03 20:41:19 +0300

    Removed bower.json from bump settings ✂

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
 
         bump: {
             options: {
-                files: ['package.json', 'bower.json'],
+                files: ['package.json'],
                 updateConfigs: ['pkg'],
                 commit: true,
                 commitMessage: 'v %VERSION%',
